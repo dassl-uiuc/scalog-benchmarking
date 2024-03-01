a92b8b24ef4ffcdeea24406a5909151d54cd5800
JiyuuuHuuu <jiyuhu1999223@gmail.com>
2024-03-01 15:30:31 -0700

    update config

--- lazylog-benchmarking/scripts/analyze.py
+++ lazylog-benchmarking/scripts/analyze.py
@@ -1,62 +1,78 @@
 import os
 import csv
+import numpy as np 
 
-def calculate_avg_throughput_latency(directory, num_bytes_per_op):
-    total_bytes_sum = 0
+def check_timeouts(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith(".csv"):
+            file_path = os.path.join(directory, filename)
+            with open(file_path, 'r') as csvfile:
+                csv_reader = csv.reader(csvfile)
+                header = next(csv_reader)
+                first_line = next(csv_reader, None)
+
+            if first_line and int(first_line[7]) != 0:
+                return True
+    
+    return False
+
+def get_latencies(directory):
+    all_latencies = []
+
+    for filename in os.listdir(directory):
+        if filename.endswith(".csv"):
+            file_path = os.path.join(directory, filename)
+            with open(file_path, 'r') as csvfile:
+                csv_reader = csv.DictReader(csvfile)
+                latencies = [int(row["latency(ns)"]) for row in csv_reader]
+                all_latencies.extend(latencies)
+    
+    return all_latencies
+
+def calculate_percentile(latencies, percentile):
+    return np.percentile(latencies, percentile)
+
+def get_latency_metrics(latencies):
+    return np.mean(latencies)/1e6, calculate_percentile(latencies, 50)/1e6, calculate_percentile(latencies, 99)/1e6
+
+def get_avg_throughput(directory, num_bytes_per_op):
+    total_bytes = 0
     max_total_time = 0
-    total_time_sum = 0
-    num_ops_total = 0
 
-    if os.path.exists(directory):
-        for filename in os.listdir(directory):
-            if filename.endswith(".csv"):
-                file_path = os.path.join(directory, filename)
+    for filename in os.listdir(directory):
+        if filename.endswith(".csv"):
+            file_path = os.path.join(directory, filename)
 
-                with open(file_path, 'r') as csvfile:
-                    csv_reader = csv.reader(csvfile)
-                    header = next(csv_reader)
-                    first_line = next(csv_reader, None)
+            with open(file_path, 'r') as csvfile:
+                csv_reader = csv.reader(csvfile)
+                header = next(csv_reader)
+                first_line = next(csv_reader, None)
+            
+            if first_line:
+                total_bytes += int(first_line[5])
+                max_total_time = max(max_total_time, int(first_line[6]))
+    
+    if max_total_time > 0:
+        return total_bytes * 1e9 / max_total_time / num_bytes_per_op
+    
+    return None
 
-                if first_line:
-                    total_bytes = int(first_line[5])
-                    total_time = int(first_line[6])
-
-                    total_bytes_sum += total_bytes
-                    max_total_time = max(max_total_time, total_time)
-                    total_time_sum += total_time
-                    num_ops_total += total_bytes/num_bytes_per_op
-
-        if max_total_time > 0:
-            avg_throughput = num_ops_total / max_total_time * 1e9
-            avg_latency = total_time_sum * 1e-6 / num_ops_total
-            return (avg_throughput, avg_latency)
-        else:
-            return (None, None)
-    return (None, None)
-
-# clients = [700, 1000, 1300, 1500, 1800, 2100, 2400]
-clients = [500,600,700,1000,1300,1500,1800]
+#clients = [2, 4, 6, 8, 16, 20, 32, 64, 128, 256, 512, 600, 700, 800, 900, 1000, 1200, 1300]
+clients = [1300,1500,1800]
 throughput = []
 latency = []
 
-print("avg_tput")
+print(f"#clients,avg tput(ops/sec),avg latency(ms/op),p50 latency(ms/op),p99 latency(ms/op)")
 for n in clients:
     # Specify the directory path
     directory_path = "../append_bench_" + str(n)
 
     # Calculate and print the average throughput
-    average_throughput, average_latency = calculate_avg_throughput_latency(directory_path, 4096)
-    throughput.append(average_throughput)
-    latency.append(average_latency)
-    print(f"{average_throughput}")
+    avg_tput = get_avg_throughput(directory_path, 4096)
+    mean, p50, p99 = get_latency_metrics(get_latencies(directory_path))
 
-print("avg_lat")
-for n in clients:
-    # Specify the directory path
-    directory_path = "../append_bench_" + str(n)
+    timeout = check_timeouts(directory_path)
+    if timeout: 
+        print("warning! timeouts detected in measurements")
 
-    # Calculate and print the average throughput
-    average_throughput, average_latency = calculate_avg_throughput_latency(directory_path, 4096)
-    throughput.append(average_throughput)
-    latency.append(average_latency)
-    print(f"{average_latency}")
+    print(f"{n},{avg_tput},{mean},{p50},{p99}")
